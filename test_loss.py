c26894eb84320f4ea6004356dccac9f261f16757
abhishek-mascon <abhishek.shukla.93@gmail.com>
2019-02-03 12:33:26 +0530

    understanding loss functions

---
+++ test_loss.py
@@ -0,0 +1,28 @@
+import chainer.functions as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+x0 = [1,0,0,1,0,0]
+x1 = [1,0,0,0,0,0]
+
+def absolute_loss(x0, x1):
+	x0 = np.array(x0, dtype=np.float32)
+	x1 = np.array(x1, dtype=np.float32)
+	#import ipdb; ipdb.set_trace()
+	loss = F.absolute_error(x0, x1)
+	plt.plot(loss.data)
+	plt.show()
+	print loss
+
+def bernoulli_nll(x, y):
+	x = np.array(x, dtype=np.float32)
+	y = np.array(y, dtype=np.float32)
+	loss = F.bernoulli_nll(x,y,reduce='no')
+	#import ipdb; ipdb.set_trace()
+	plt.plot(loss.data)
+	plt.show()
+	print loss
+
+absolute_loss(x0, x1)
+#bernoulli_nll(x0, x1)
+
